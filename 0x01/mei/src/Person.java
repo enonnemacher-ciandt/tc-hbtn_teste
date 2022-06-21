6a6246dfd3d6d61f946ac9060b40f6b87bb4e74b
enonnemacher-ciandt <enonnemacher@ciandt.com>
2022-06-21 15:00:44 -0300

    Task 1-MEI

---
+++ 0x01/mei/src/Person.java
@@ -0,0 +1,57 @@
+package task1mei.src;
+
+import java.time.Instant;
+import java.util.Date;
+
+public class Person {
+
+    private String name;
+    private String surname;
+    private Date birthDate;
+    private boolean anotherCompanyOwner;
+    private boolean pensioner;
+    private boolean publicServer;
+    private float salary;
+
+    public Person(String name, String surname, Date birthDate, boolean anotherCompanyOwner, boolean pensioner, boolean publicServer) {
+        this.name = name;
+        this.surname = surname;
+        this.birthDate = birthDate;
+        this.anotherCompanyOwner = anotherCompanyOwner;
+        this.pensioner = pensioner;
+        this.publicServer = publicServer;
+    }
+
+    public String fullName() {
+        return this.name + " " + this.surname;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public void setAnotherCompanyOwner(boolean anotherCompanyOwner) {
+        this.anotherCompanyOwner = anotherCompanyOwner;
+    }
+
+    public void setPensioner(boolean pensioner) {
+        this.pensioner = pensioner;
+    }
+
+    public void setPublicServer(boolean publicServer) {
+        this.publicServer = publicServer;
+    }
+
+    public float calculateYearlySalary() {
+        return getSalary() * 12;
+    }
+
+    public boolean isMEI() {
+        long age = Date.from(Instant.now()).getTime() - this.birthDate.getTime();
+        return calculateYearlySalary() < 130000 && age > 18 && !this.anotherCompanyOwner && !this.pensioner && !this.publicServer;
+    }
+}
---
+++ 0x01/mei/src/PersonTest.java
@@ -0,0 +1,50 @@
+package task1mei.src;
+
+import org.junit.jupiter.api.*;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PersonTest {
+
+    Person person;
+
+    @BeforeEach
+    public void setup() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.clear();
+        calendar.set(Calendar.YEAR, 2000);
+        Date date = calendar.getTime();
+        person = new Person("Paul", "McCartney", date, true, true, true);
+    }
+
+    @Test
+    @DisplayName("Deve retornar nome completo")
+    public void show_full_name() {
+        assertEquals("Paul McCartney", person.fullName());
+    }
+
+    @Test
+    @DisplayName("Deve retornar o salario anual")
+    public void test_calculateYearlySalary() {
+        person.setSalary(1200);
+        assertEquals(14400, person.calculateYearlySalary());
+    }
+
+    @Test
+    @DisplayName("Deve retornar TRUE se pessoa isMEI")
+    public void person_is_MEI() {
+        person.setAnotherCompanyOwner(false);
+        person.setPensioner(false);
+        person.setPublicServer(false);
+        assertTrue(person.isMEI());
+    }
+
+    @Test
+    @DisplayName("Deve retornar FALSE se pessoa isNotMEI")
+    public void person_is_not_MEI() {
+        assertFalse(person.isMEI());
+    }
+}
